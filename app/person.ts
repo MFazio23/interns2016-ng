b4d471ea0a07b92ebc8c6359e0a5e557ccee0a8d
Michael Fazio <mfazio@skylinetechnologies.com>
2016-06-09 14:13:39 -0500

    Add person class.

---
+++ app/person.ts
@@ -0,0 +1,13 @@
+export class Person {
+    name: string;
+    title: string;
+    school: string;
+    email: string;
+
+    constructor(name: string, title: string, school: string, email: string) {
+        this.name = name;
+        this.title = title;
+        this.school = school;
+        this.email = email;
+    }
+}

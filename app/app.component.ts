14540cde903abbcd678933ca0b0a811337efda97
Michael Fazio <mfazio@skylinetechnologies.com>
2016-06-09 10:05:38 -0500

    Add items to component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from '@angular/core';
+import {Routes, ROUTER_DIRECTIVES} from "@angular/router";
+
+@Routes([
+    {path: '/', component: ListUsersComponent},
+    {path: '/edit?userId=:id', component: EditUserComponent},
+    {path: '/create', component: CreateUserComponent},
+    {path: '/view?userId=:id', component: ViewUserComponent}
+])
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    directives: [ROUTER_DIRECTIVES],
+    template:
+`
+<top-nav></top-nav>
+<router-outlet></router-outlet>
+<footer></footer>
+`
 })
 export class AppComponent { }

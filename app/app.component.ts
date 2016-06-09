c35edcf08bf05d6133652a8e504ae30b5988ed8d
Brown <cbrown@skylinetechnologies.com>
2016-06-09 11:04:03 -0500

    no message

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {EditUserComponent} from './edituser.component';
 import {Routes, ROUTER_DIRECTIVES} from "@angular/router";
+import {ViewUserComponent}} from './ViewUserComponent';
 
 @Routes([
-   // {path: '/', component: ListUsersComponent},
+    //{path: '/', component: ListUsersComponent},
     {path: '/edit?userId=:id', component: EditUserComponent}
-   // {path: '/create', component: CreateUserComponent},
-   // {path: '/view?userId=:id', component: ViewUserComponent}
+   //{path: '/create', component: CreateUserComponent},
+  {path: '/view?userId=:id', component: ViewUserComponent}
 ])
 
 @Component({

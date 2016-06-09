0a0197ea65e37d33fcc5dd0d67ba2d649bec9b0e
Dopkins <ndopkins@skyline.gb>
2016-06-09 10:42:08 -0500

    Create User added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {Routes, ROUTER_DIRECTIVES} from "@angular/router";
+import {CreateUserComponent} from './createuser.component';
 
 @Routes([
-    {path: '/', component: ListUsersComponent},
-    {path: '/edit?userId=:id', component: EditUserComponent},
+    //{path: '/', component: ListUsersComponent},
+    //{path: '/edit?userId=:id', component: EditUserComponent},
     {path: '/create', component: CreateUserComponent},
-    {path: '/view?userId=:id', component: ViewUserComponent}
+    //{path: '/view?userId=:id', component: ViewUserComponent}
 ])
 
 @Component({
---
+++ app/createuser.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'create-user',
+    template:
+`
+
+<h1>Test</h1>
+
+`
+})
+export class CreateUserComponent { }

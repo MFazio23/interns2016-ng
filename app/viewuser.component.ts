b0a48e252497eae4f54a2811a867481803fecbd5
Brown <cbrown@skylinetechnologies.com>
2016-06-09 14:11:00 -0500

    no message

--- app/viewuser.component.ts
+++ app/viewuser.component.ts
@@ -12,20 +12,19 @@ import {Router, Routes, ROUTER_DIRECTIVES} from "@angular/router";
     <h1>View User</h1>
   </title>
 </header>
-  <div class="container">
-    <div class="wrapper">
-      <div class="faceAndName">
+  <div class="container-fluid">
+      <div class="col-xs-12 col-md-8">
         <img src="#">
         <h2>NamePlace</h2>
       </div>
-      <div class="info">
+      <div class="col-xs-6 col-md-4">
         <ul>
           <li>Position: </li>
           <li>School</li>
           <li>Email <small>email for good time!</small></li>
         </ul>
+        <a href="index.html">Back</a>
       </div>
-    </div>
   </div>
   
 <top-nav></top-nav>

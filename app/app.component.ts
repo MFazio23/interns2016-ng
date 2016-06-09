49b6efcd026e4fc375a814e56285b5112629a43e
Brown <cbrown@skylinetechnologies.com>
2016-06-09 14:58:18 -0500

    no message

--- app/app.component.ts
+++ app/app.component.ts
@@ -18,39 +18,9 @@ import {ViewUserComponent} from './viewuser.component';
     template:
 `
 
-    <!-- Navigation -->
-    <router-outlet></router-outlet>
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="/view">View</a>
-                    </li>
-                    <li>
-                        <a href="/edit?userId=:id">Edit</a>
-                    </li>
-                    <li>
-                        <a href="/create">Create</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
+    <router-outlet>
+        </router-outlet>
     <!-- Page Content -->
     <div class="container">
 
@@ -112,28 +82,7 @@ import {ViewUserComponent} from './viewuser.component';
                 <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
             </div>
         </div>
-
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+        </div>
 `
 })
 export class AppComponent {
